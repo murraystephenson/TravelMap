54f5c747c6771329029a58d2f83b9644f7dba1cf
murraystephenson <murraystephenson@hotmail.com>
2025-08-20 10:28:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Initialize map centered on the world
+// Initialize map
 const map = L.map('map').setView([20, 0], 2);
 
 // Add OpenStreetMap tiles
@@ -12,22 +12,22 @@ const towns = [
   { name: "Cape Town, South Africa", coords: [-33.918, 18.423], date: "2022-12-05" }
 ];
 
-// Add towns as markers
 towns.forEach(town => {
   L.marker(town.coords)
     .addTo(map)
     .bindPopup(`📍 ${town.name} <br>📅 ${town.date}`);
 });
 
-// ----- Dummy Countries -----
-const countries = [
-  { name: "Botswana", coords: [[[-25, 20], [-25, 30], [-15, 30], [-15, 20], [-25, 20]]], year: "2023" },
-  { name: "South Africa", coords: [[[-35, 16], [-35, 32], [-28, 32], [-28, 16], [-35, 16]]], year: "2022" }
-];
-
-// Add countries as polygons
-countries.forEach(country => {
-  L.polygon(country.coords, { color: 'blue', fillColor: 'lightblue', fillOpacity: 0.4 })
-    .addTo(map)
-    .bindPopup(`🌐 ${country.name} <br>📅 First visited: ${country.year}`);
-});
+// ----- Countries -----
+fetch('data/countries.geojson')
+  .then(res => res.json())
+  .then(geojson => {
+    L.geoJSON(geojson, {
+      style: {
+        color: 'blue',
+        fillColor: 'lightblue',
+        fillOpacity: 0.4,
+        weight: 1
+      }
+    }).addTo(map);
+  });
