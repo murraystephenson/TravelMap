135c729cf2a2373f06fd3956de7285818dc57ec0
murraystephenson <murraystephenson@hotmail.com>
2025-08-20 12:51:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,23 +17,12 @@ const yearFilter = document.getElementById('yearFilter');
 // -----------------------------
 const towns = [
   { City: 'Maun', Country: 'Botswana', Latitude: -19.983, Longitude: 23.431, Date: '2020-11-06', Years: ['2020', '2021', '2022', '2023', '2024, '2025'] },
-  { City: 'Cape Town', Country: 'South Africa', Latitude: -33.918, Longitude: 18.423, Date: '2022-12-05', Years: ['1987'] }
-  { City: 'London', Country: 'England', Latitude: -51.507, Longitude: 0.127, Date: '2017-07-26', Years: ['2017'] }
-  { City: 'Boston', Country: 'United States of America', Latitude: -42.356, Longitude: 71.056, Date: '2017-07-18', Years: ['2017'] }
 ];
 
 // Local data for countries visited (derived from towns)
 const visitedCountries = [
   { name: 'Botswana', year: 2020 },
   { name: 'Botswana', year: 2021 },
-  { name: 'Botswana', year: 2022 },
-  { name: 'Botswana', year: 2023 },
-  { name: 'Botswana', year: 2024 },
-  { name: 'England', year: 2017 },
-  { name: 'United States of America', year: 2017 },
-  { name: 'Denmark', year: 2017 },
-  { name: 'Belgium', year: 2017 },
-  { name: 'South Africa', year: 2022 }
 ];
 
 // -----------------------------
