b72dda3f9b02550d89761bfe291ebb40aecdd5c6
murraystephenson <murraystephenson@hotmail.com>
2025-08-20 16:06:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ let countryLayers = [];
 function addMarkers() {
   markers = locations.map(loc => {
     const marker = L.marker([loc.lat, loc.lng])
-      .bindPopup(`📍 ${loc.city}, ${loc.country});
+      .bindPopup(`📍 ${loc.city}, ${loc.country}`);
     marker.years = loc.years;
     marker.addTo(map);
     return marker;
